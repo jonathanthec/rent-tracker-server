52a48f3e11d9dd1ba3b508d057f6e7dbb2630f02
JonathanChen <jonny29009@yahoo.com>
2020-04-25 00:15:50 -0700

    basic graphql server

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rent Tracker Design File
+
+This is the design file for rent tracker backend, which will be built using GraphQL.
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-const { GraphQLServer } = require('graphql-yoga');
-const { prisma } = require('./generated/prisma-client');
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-
+const resolvers = {
+    Query: {
+        feed: (root, args, context, info) => {
+            return context.prisma.properties();
+        },
+    },
+    Mutation: {
+        createProperty: (root, args, context) => {
+            return context.prisma.createProperty({
+                address: args.address,
+            })
+        },
+    },
+}
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
+    context: { prisma }
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
